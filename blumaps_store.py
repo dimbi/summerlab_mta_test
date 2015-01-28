af0fe1916fae6ebf1a6b9607e4c688a89efeacab
drp <dimasrinarso2003@yahoo.com>
2015-01-28 15:18:46 -0500

    store RDBMS way

--- blumaps_store.py
+++ blumaps_store.py
@@ -35,83 +35,28 @@ def WebAccess():
     else:
       sys.exit("Received an error from server, cannot retrieve results " + str(webUrl.getcode()))   
     return data
-       
-def printResults(data):
-  #declaration of local variables
-  pro=[]
-
-  #Preparation for conversion
-  proj=pyproj.Proj(init="esri:26918")
-  for i in busJson["Siri"]["ServiceDelivery"]["VehicleMonitoringDelivery"]:
-    for j in i["VehicleActivity"]:
-        lat=j["MonitoredVehicleJourney"]["VehicleLocation"]["Latitude"]
-        lon=j["MonitoredVehicleJourney"]["VehicleLocation"]["Longitude"]
-        pro.append(proj(lon,lat)) 
-
-  return pro
- 
+        
 def storePostgre(pgData):
 
   # Use the json module to load the string data into a dictionary
   jsonData = json.loads(pgData)  
-  
-  #hardcode
-  keyIdx = 10
+
   #connecting to postGres and write jsonData
-  #try:
   db = psycopg2.connect('dbname=json_test user=dimasrinarso password=bibirkubiasasaja')
   cur = db.cursor()
-  cur.execute("INSERT INTO bus(id,data) VALUES (%s,%s)",(keyIdx,json.dumps(jsonData)))
+
+  #Preparation for conversion
+  keyIdx = -1
+  for i in jsonData["Siri"]["ServiceDelivery"]["VehicleMonitoringDelivery"]:
+    for j in i["VehicleActivity"]:
+      keyIdx =+ 1
+      lineName=j["MonitoredVehicleJourney"]["PublishedLineName"]
+      destName=j["MonitoredVehicleJourney"]["DestinationName"]
+      lat=j["MonitoredVehicleJourney"]["VehicleLocation"]["Latitude"]
+      lon=j["MonitoredVehicleJourney"]["VehicleLocation"]["Longitude"]
+      timeStamp=j["RecordedAtTime"]
+      cur.execute("INSERT INTO bus(id,time,line,destination,latitude,longitude) VALUES (%d,%s,%s,%s,%s,%s)",(keyIdx,timeStamp,lineName,destName,lat,lon))
   db.commit()
-
-  #except psycopg2.Error as e:
-  #  sys.exit("Error when connecting or writing to postGres database = %s : %s", (e.pgcode,e.pgerror))
-      
-def printMap(shpdir,outpdf,coord):
-
-    #basic plot setting
-    fig = plt.figure(figsize=(4.5, 7.2))
-    fig.suptitle('Current Bus Locations', fontsize=20)
-    ax = fig.add_subplot(111, aspect='equal')
-    
-    #read .shp file
-    sf = shapefile.Reader(shpdir)
-    
-    #iterating through each records
-    for sr in sf.shapeRecords():
-        color = '0.8'
-        if sr.record[1]!='      ':
-            color='orange'
-            
-        parts = list(sr.shape.parts) + [-1]
-            
-        for i in xrange(len(sr.shape.parts)):
-            path = Path(sr.shape.points[parts[i]:parts[i+1]])
-            patch = PathPatch(path, edgecolor=color, facecolor='none', lw=0.5, aa=True)
-            ax.add_patch(patch)
-    
-    #plot the bus projection coordinates onto the rendered maps
-    #matplotlib interactive mode 
-    #plt.ion()
-    #plotting points
-
-    #infinite repetition
-    #while True:
-    for i in xrange(len(coord)):
-        ax.plot(coord[i][0],coord[i][1],'bo',markersize=6)
-        ax.set_xlim(sf.bbox[0], sf.bbox[2])
-    ax.set_ylim(sf.bbox[1], sf.bbox[3])
-    ax.xaxis.set_major_locator(MaxNLocator(3))   
-
-    plt.show()
-
-    #sleep 5 secs
-    #time.sleep(5)
-
-    #DEBUG: printing to PDF
-    #fig.savefig(outpdf)
-    #print("Finished created : "+outpdf);
-
 
 def main(bus_key,shp_dir,out_pdf):
 
