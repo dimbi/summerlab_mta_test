30e10ff8c5fe7151508264d9cd81ba96e1b36c37
drp <dimasrinarso2003@yahoo.com>
2015-01-29 19:31:36 -0500

    minor fixes

--- blumaps_store_offline.py
+++ blumaps_store_offline.py
@@ -24,32 +24,34 @@ def storePostgre(dataDir, fileName):
   cur = db.cursor()
 
   #iterating each line from offline bus data  
+  keyIdx = -1
   for line in f:
     tokenCount = -1
+    keyIdx += 1
     line = line.strip()
     for tokens in line.split('\t'):
       tokenCount+=1
-      if tokenCount = 0: 
+      if tokenCount == 0: 
         lat = tokens
-      elif tokenCount = 1:
+      elif tokenCount == 1:
         lon = tokens
-      elif tokenCount = 2:
+      elif tokenCount == 2:
         timeStamp = tokens
-      elif tokenCount = 3:
-        line = tokens
-      elif tokenCount = 4:
-        distance = tokens      
-      elif tokenCount = 10:
-        destinationName = tokens
- 
+      elif tokenCount == 3:
+        lineName = tokens
+      elif tokenCount == 4:
+        distancePerLine = tokens      
+      elif tokenCount == 10:
+        destName = tokens
+
   # Use the json module to load the string data into a dictionary
-    print keyIdx
-    cur.execute("INSERT INTO bus(id,time,line,destination,latitude,longitude,distance) VALUES (%s,%s,%s,%s,%s,%s,%s)",(keyIdx,timeStamp,lineName,destName,lat,lon, destinationName))
+    cur.execute("INSERT INTO bus(id,time,line,destination,latitude,longitude,distance) VALUES (%s,%s,%s,%s,%s,%s,%s)",(keyIdx,timeStamp,lineName,destName,lat,lon, distancePerLine))
   db.commit()
 
 def main():
   fileName = "MTA-Bus-Time_."
   offlineDir = "offline_data"   
+
   storePostgre(offlineDir,fileName)
       
 if __name__ == '__main__':
