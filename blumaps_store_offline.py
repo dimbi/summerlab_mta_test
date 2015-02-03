cf73c938c4060777cf34bacec6b1cb3d8584c6ae
dimbi <dimasrinarso2003@yahoo.com>
2015-02-03 11:17:36 -0500

    fixing size

--- blumaps_store_offline.py
+++ blumaps_store_offline.py
@@ -14,7 +14,7 @@ import psycopg2.extras
 from datetime import date,datetime,timedelta
 
 #global init
-start_date='2014-08-02'
+start_date='2014-08-01'
 end_date='2014-10-31'
 startDate = datetime.strptime(start_date,"%Y-%m-%d")
 endDate = datetime.strptime(end_date,"%Y-%m-%d")
@@ -23,56 +23,64 @@ def daterange(startDate, endDate):
   for n in range(int ((endDate - startDate).days)):
     yield startDate + timedelta(n)
 
-        
+  
+notInList = ["2014-10-06","2014-10-17"]
+      
 def storePostgre(dataDir, fileName):
 
   #iterating each line from offline bus data  
   keyIdx = -1
   
   for singleDate in daterange(startDate, endDate):
+    if (singleDate.strftime("%Y-%m-%d")) in notInList: pass
 
-    #open each data
-    f = open(dataDir+"/"+fileName+(singleDate.strftime("%Y-%m-%d"))+".txt")
+    #debug only
+    #if (singleDate.strftime("%Y-%m-%d")) == "2014-08-02":
+    #    break
 
-    #connecting to postGres and write jsonData
-    db = psycopg2.connect('dbname=bus_data user=postgres password=postgres')
-    cur = db.cursor()
+    else:
+      #open each data
+      f = open(dataDir+"/"+fileName+(singleDate.strftime("%Y-%m-%d"))+".txt")
 
-    #tempCounter = -1
-    headerIdx = -1
-    for line in f:
-      #tempCounter+=1
-      tokenCount = -1
-      keyIdx += 1
-      headerIdx += 1
-      if headerIdx == 0:
-        pass
-      else:
-        line = line.strip()
-        for tokens in line.split('\t'):
-          tokenCount+=1
-          if tokenCount == 0: 
-            lat = tokens
-          elif tokenCount == 1:
-            lon = tokens
-          elif tokenCount == 2:
-            timeStamp = tokens
-          elif tokenCount == 3:
-            vehicleId = tokens
-          elif tokenCount == 4:
-            distancePerLine = tokens
-          elif tokenCount == 7:
-            lineName = tokens      
-      
-          elif tokenCount == 10:
-            destName = tokens
-        # Use the json module to load the string data into a dictionary
-        cur.execute("INSERT INTO bus(id,time,line,destination,latitude,longitude,distance,vehicle_id) \
-                     VALUES (%s,%s,%s,%s,%s,%s,%s,%s)",
-                    (keyIdx,timeStamp,lineName,destName,lat,lon,distancePerLine,vehicleId))
-    #if tempCounter==5:
-    #  break
-  db.commit()
+      #connecting to postGres and write jsonData
+      db = psycopg2.connect('dbname=bus_data user=postgres password=postgres')
+      cur = db.cursor()
+
+      tempCounter = -1
+      headerIdx = -1
+      for line in f:
+        tempCounter+=1
+        tokenCount = -1
+        keyIdx += 1
+        headerIdx += 1
+        if headerIdx == 0:
+          pass
+        else:
+          line = line.strip()
+          for tokens in line.split('\t'):
+            tokenCount+=1
+            if tokenCount == 0: 
+              lat = tokens
+            elif tokenCount == 1:
+              lon = tokens
+            elif tokenCount == 2:
+              timeStamp = tokens
+            elif tokenCount == 7:
+              lineName = tokens      
+            elif tokenCount == 10:
+              destName = tokens
+
+          # Use the json module to load the string data into a dictionary
+          cur.execute("INSERT INTO bus(id,timestamp,line,destination,latitude,longitude) \
+                       VALUES (%s,%s,%s,%s,%s,%s)",
+                      (keyIdx,timeStamp,lineName,destName,lat,lon))
+
+          #For debug purpose only
+          #if tempCounter==10:
+          #  break
+ 
+      db.commit()
+      print "data on date "+(singleDate.strftime("%Y-%m-%d"))+" inserted successfully."
 
 def main():
   fileName = "MTA-Bus-Time_."
